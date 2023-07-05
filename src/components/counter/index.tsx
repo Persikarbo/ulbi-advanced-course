d1a5f492856b2c4bd7706373c2b53c4e16784597
Khlobystova Alina <khlobystovaae@gmail.com>
2023-07-05 21:32:36 +0500

    Refactor counter component

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -5,9 +5,7 @@ export const Counter = () => {
 
     const [ count, setCount ] = useState(0);
 
-    const onClick = () => {
-        setCount(count + 1);
-    }
+    const onClick = () => setCount(count + 1);
 
     return (
         <div className={classes.counter}>
