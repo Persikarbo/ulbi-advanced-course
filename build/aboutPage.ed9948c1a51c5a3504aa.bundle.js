6c01e06d6ac6bfb2b7084b9c3ea202531599f343
Khlobystova Alina <khlobystovaae@gmail.com>
2023-10-06 00:47:59 +0500

    Build

---
+++ build/aboutPage.ed9948c1a51c5a3504aa.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkulbi_advanced_course=self.webpackChunkulbi_advanced_course||[]).push([[170],{3417:(e,u,c)=>{c.r(u),c.d(u,{default:()=>a});var n=c(5893),s=c(8749);const a=function(){return(0,n.jsx)(s.X,{children:"ABOUT PAGE"},void 0)}}}]);
